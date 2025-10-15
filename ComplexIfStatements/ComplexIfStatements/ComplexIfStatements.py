b3919ac9b14e64c0c6774e3ebc7d3de1a67b79fc
Tiffany Womack <malice_disciple@live.com>
2025-10-15 17:37:53 -0500

    Revert "added more sports options by uncommenting remaining code"
    This reverts commit 6b526bd53c9ff9bbb5b3d9f83f78c6e8494741ef.

--- ComplexIfStatements/ComplexIfStatements/ComplexIfStatements.py
+++ ComplexIfStatements/ComplexIfStatements/ComplexIfStatements.py
@@ -7,33 +7,33 @@ if sport == 'HOCKEY':
         print('Good luck getting to the cup')
         print('We do love hockey!!')
 
-sportIsHockey = False
-if sport == 'HOCKEY':
-   sportIsHockey = True
+#sportIsHockey = False
+#if sport == 'HOCKEY':
+#    sportIsHockey = True
 
-teamIsCorrect = False
-if team == 'SENATORS' or team == 'LEAFS':
-   teamIsCorrect = True
+#teamIsCorrect = False
+#if team == 'SENATORS' or team == 'LEAFS':
+#    teamIsCorrect = True
 
-if sportIsHockey and teamIsCorrect:
-    print('Good luck getting to the cup this year')
+#if sportIsHockey and teamIsCorrect:
+#     print('Good luck getting to the cup this year')
 
-#If the sport is hockey, and the team is the senators or leafs, display the cup message
-if sport == 'HOCKEY' and (team == 'SENATORS' or team == 'LEAFS'):
-    print('Good luck getting to the cup this year')
+##If the sport is hockey, and the team is the senators or leafs, display the cup message
+#if sport == 'HOCKEY' and (team == 'SENATORS' or team == 'LEAFS'):
+#     print('Good luck getting to the cup this year')
 
-if sport == 'HOCKEY' and team == 'RANGERS':
-   print('I miss Messier')
-elif team == 'LEAFS' or team == 'SENATORS':
-   print('Good luck getting the cup this year')
-else:
-   print('I don\'t know that team')
+#if sport == 'HOCKEY' and team == 'RANGERS':
+#    print('I miss Messier')
+#elif team == 'LEAFS' or team == 'SENATORS':
+#    print('Good luck getting the cup this year')
+#else:
+#    print('I don\'t know that team')
 
-if team == 'FLYERS':
-   print('Best team ever!!')
-elif team == 'SENATORS':
-   print('Go Sens Go!')
-elif team == 'RANGERS':
-   print('Go Rangers')
-else:
-   print('I don\'t have anything clever to say here')
+#if team == 'FLYERS':
+#    print('Best team ever!!')
+#elif team == 'SENATORS':
+#    print('Go Sens Go!')
+#elif team == 'RANGERS':
+#    print('Go Rangers')
+#else:
+#    print('I don\'t have anything clever to say here')
